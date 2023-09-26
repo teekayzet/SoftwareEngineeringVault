750b24888532ad552d6e6cfb57e8f866dd4b93c2
Takudzwa Mutamburigwa <122125273+teekayzet@users.noreply.github.com>
2023-09-26 23:04:47 +0200

    All codes uploaded

--- Atm_Simulator/ATMCaseStudy.java
+++ Atm_Simulator/ATMCaseStudy.java
@@ -1,3 +1,14 @@
-public class ATMCaseStudy {
-    
-}
+// ATMCaseStudy.java
+// Driver program for the ATM case study
+
+public class ATMCaseStudy
+{
+   // main method creates and runs the ATM
+   public static void main(String[] args)
+   {
+	  
+      ATM theATM = ATM.getinstance();
+      
+      theATM.run();
+   } // end main
+} // end class ATMCaseStudy 
--- Atm_Simulator/Iterator.java
+++ Atm_Simulator/Iterator.java
@@ -1,3 +1,8 @@
-public class Iterator {
-    
+
+public interface Iterator {
+	//This is the interface for the Iterator pattern.
+		boolean hasNext(int position);
+		Object next(int position);
+		boolean hasPrev(int position);
+	
 }
